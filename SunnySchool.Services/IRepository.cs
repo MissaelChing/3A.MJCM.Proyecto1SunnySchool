a4b7d6de4276ce9c246f2cb40d1d3a2a7a07c767
MissaelChing <Missa_ching@hotmail.com>
2020-06-26 10:30:32 -0500

    Nuevos Cambios

---
+++ SunnySchool.Services/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SunnySchool.Models;
+
+namespace SunnySchool.Services
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public IEnumerable<T> GetAll();
+        public T Get(int id);
+        public int Insert(T obj);
+        public void Update(T obj);
+        public void Delete(T obj);
+
+    }
+}
---
+++ SunnySchoolUI/Pages/FormularioBeca.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SunnySchool.Models;
+using SunnySchool.Services;
+
+namespace SunnySchoolUI.Pages
+{
+    public class FormularioBecaModel : PageModel
+    {
+        [BindProperty]
+        public SolicitudM solicitudM{ get; set; }
+        public readonly IRepository<SolicitudM> repositorysolicitar;
+        public FormularioBecaModel(IRepository<SolicitudM> repositorysolicitar)
+        {
+            this.repositorysolicitar = repositorysolicitar;
+
+        }
+        
+        public IActionResult OnPost()
+        {
+            repositorysolicitar.Insert(solicitudM);
+            return Redirect("/Pages/Index?id=" + solicitudM);
+        }
+    }
+}
