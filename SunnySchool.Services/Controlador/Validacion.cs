0e1bd98b51d12210cf2f21de045931316d5d8885
MissaelChing <Missa_ching@hotmail.com>
2020-07-29 20:49:28 -0500

    actualizaciones

---
+++ SunnySchool.Services/Controlador/Validacion.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SunnySchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SunnySchool.Services.Controlador
+{
+    public class Validacion : SQLRepository<Users>, IRepositoryValidar
+    {
+        
+        public Validacion(AppDBContext context) : base (context) { }
+        public IEnumerable<Users> GetUsers()
+        {
+            return context.Set<Users>().AsEnumerable();
+        }
+
+        public AlumnoM getusuarios(int id)
+        {
+
+            return context.Alumno.Include(x=>x.Id).FirstOrDefault(x => x.Id == id);
+
+        }
+        public EvaluadorM getusuariosE(int id)
+        {
+
+            return context.Evaluador.Include(x => x.Id).FirstOrDefault(x => x.Id == id);
+
+        }
+       
+        public int Validar(Users users)
+        {
+            var cuenta2 = context.Users.ToList();
+            int i = 0;
+
+            foreach (var items in cuenta2)
+            {
+                if(users.Usuario == items.Usuario && users.Contraseña == items.Contraseña)
+                {
+                    i = items.Id;
+                    break;
+                }
+               
+            } 
+            return i; 
+        }
+        
+    }
+}
+     
+       
+    
+
---
+++ SunnySchoolUI/Pages/FormularioBeca.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SunnySchool.Models;
+using SunnySchool.Services;
+
+namespace SunnySchoolUI.Pages
+{
+    public class FormularioBecaModel : PageModel
+    {
+        [BindProperty]
+        public SolicitudM solicitudM { get; set; }
+        public AlumnoM alumnoM { get; set; }
+        public IRepositorySolicitud repositorysolicitud;
+        public IRepositoryAlumno repositoryalumno;
+
+        public FormularioBecaModel(IRepositorySolicitud repositorysolicitud, IRepositoryAlumno repositoryalumno)
+        {
+            this.repositorysolicitud = repositorysolicitud;
+            this.repositoryalumno = repositoryalumno;
+
+        }
+        public IActionResult OnPost()
+        {
+            int a ;
+          a =  repositorysolicitud.InsertS(solicitudM);
+            if(a==1)
+            {
+                alumnoM.Nombre = solicitudM.Nombrealumno;
+                alumnoM.SolicitudId = solicitudM.Id;
+                repositoryalumno.InsertA(alumnoM);
+            }
+            return Redirect("/FormularioBeca");
+
+
+        }
+
+    }
+
+}
